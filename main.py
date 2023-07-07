c8e531fd7d8856da0082daacfca961813a5cf374
Varun Mannam <varun.mannam18@gmail.com>
2023-07-07 15:51:48 -0400

    finished design of chess board and added rank/file values

--- main.py
+++ main.py
@@ -9,10 +9,33 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
 board = pygame.Surface((664, 664))
-board.fill((255, 206, 158))
+board.fill((227,193,111))
+
+font = pygame.font.Font('freesansbold.ttf', 20)
+ 
+
+for y in range(8):
+    if(y%2==1):
+        for x in range(0, 8, 2):
+            pygame.draw.rect(board, (184,139,74), (x*83, y*83, 83, 83))
+    else:
+        for x in range(0, 8, 2):
+            pygame.draw.rect(board, (184,139,74), ((x+1)*83, y*83, 83, 83))
 
 screen.blit(board, (68, 68))
+
+for i in range(8):
+    text = font.render(files[i], True, (255,255,255))
+    textRect = text.get_rect()
+    textRect.topleft = (48+83*(i+1), 709)
+    screen.blit(text, textRect)
+    text = font.render(str(i+1), True, (255,255,255))
+    textRect = text.get_rect()
+    textRect.topleft = (74, 83*(i+1)-8)
+    screen.blit(text, textRect)
 
 while running:
     # poll for events
